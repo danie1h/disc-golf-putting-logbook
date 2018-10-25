80621e5c40a7ecb4a8379e78d73e1de27d424893
Daniel Humphreys <dhumphreys.co@gmail.com>
2018-10-24 21:07:12 -0700

    Updates

---
+++ precache-manifest.838c9f305b4df7f32c6742821bff497f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1ec00158b33d5a1386e",
+    "url": "/disc-golf-putting-logbook/static/js/runtime~main.d1ec0015.js"
+  },
+  {
+    "revision": "6ed3ea78e0278c135b9e",
+    "url": "/disc-golf-putting-logbook/static/js/main.6ed3ea78.chunk.js"
+  },
+  {
+    "revision": "aba538ea34ea4dfa937f",
+    "url": "/disc-golf-putting-logbook/static/js/1.aba538ea.chunk.js"
+  },
+  {
+    "revision": "6ed3ea78e0278c135b9e",
+    "url": "/disc-golf-putting-logbook/static/css/main.2ad4b89f.chunk.css"
+  },
+  {
+    "revision": "402c71bf9e841803556cb72697ec4aa1",
+    "url": "/disc-golf-putting-logbook/index.html"
+  }
+];
