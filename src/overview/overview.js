14633b0100e18dd8b50fbf343dc24afd1bd199ce
Daniel Humphreys <dhumphreys.co@gmail.com>
2018-10-26 13:28:42 -0700

    add overview data table

--- src/overview/overview.js
+++ src/overview/overview.js
@@ -1,33 +1,44 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { Stage, Layer, Circle, Star } from 'react-konva'
+import { Stage, Layer } from 'react-konva'
 import './overview.css'
 
 class PuttOverview extends Component {
   render() {
-    let puttDetailList = this.props.puttLog.map( putt => {
-      return(<p key={putt.holeNum}>Hole #{putt.holeNum}: {putt.mode} </p>)
+    let puttDataList = this.props.puttLog.map( putt => {
+      return(
+        <tr key={putt.holeNum}>
+          <td>{putt.holeNum}</td>
+          <td>{putt.mode}</td>
+        </tr>
+      )
     })
 
     return (
       <div className='putting-overview'>
         <h1 className='title'>Overview</h1>
-        <h2 className='description'>More features and stats coming soon!</h2>
         {
-          (puttDetailList.length > 0) ?
+          (puttDataList.length > 0) ?
             <div>
-              <div className="canvas-container">
+              <div className='canvas-container'>
                 <Stage width={this.props.canvasWidth} height={this.props.canvasHeight}>
                   <Layer>
                     {this.props.puttCanvasShapes}
                   </Layer>
                 </Stage>
               </div>
-              {puttDetailList}
+              <table className='data-table'>
+                <tr>
+                  <th>Hole #</th>
+                  <th>Putt Attempt</th>
+                </tr>
+                {puttDataList}
+              </table>
             </div>
           :
             <p>No Data Available. Log your putts <Link to='/disc-golf-putting-logbook/log'>here</Link>, then check your overview.</p>
         }
+        <p className='coming-soon'>More features and stats coming soon!</p>
       </div>
     )
   }
