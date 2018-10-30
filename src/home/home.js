66bd00b81359e70140a9f185b7496ee9be34383b
Daniel Humphreys <dhumphreys.co@gmail.com>
2018-10-30 12:36:38 -0700

    revamp home page

--- src/home/home.js
+++ src/home/home.js
@@ -6,7 +6,7 @@ export const Home = () => {
   return (
     <div className='home'>
       <h1 className='title'>Disc Golf Putting Logbook</h1>
-      <h2 className='description'>Log Putts, View Stats, Learn and Improve, Repeat</h2>
+      <h2 className='description'>Log Putts, View Stats, Improve Your Game, Repeat</h2>
       <div className='home-link-items'>
         <Link to='/disc-golf-putting-logbook/log' id='get-started' className='home-link'>Get Started</Link>
         <Link to='/disc-golf-putting-logbook/about' id='learn-more' className='home-link'>Learn More</Link>
