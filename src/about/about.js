53f5607d897c8b89c6473c733376adf333ece670
Daniel Humphreys <dhumphreys.co@gmail.com>
2018-10-30 11:19:51 -0700

    Open links in new tab

--- src/about/about.js
+++ src/about/about.js
@@ -9,7 +9,7 @@ export const About = () => {
       </header>
       <hr className='divider' />
       <section className='overview'>
-        <p className='description'>In the sport of <a href='https://www.pdga.com/introduction'>Disc Golf</a>, putting is an essential aspect and, arguably, one of the greatest challenges in a round. It's not uncommon for players to frequently miss their mark of the basket. Consistency is key, and this project will enable disc golfers to track their putting consistency. By identifying patterns, players will have the ability to leverage their stats to make the necessary modifications that will make them a better putter and, in turn, an improved player. Cue the Putting Logbook.</p>
+        <p className='description'>In the sport of <a href='https://www.pdga.com/introduction' target='_blank' rel='noopener noreferrer'>Disc Golf</a>, putting is an essential aspect and, arguably, one of the greatest challenges in a round. It's not uncommon for players to frequently miss their mark of the basket. Consistency is key, and this project will enable disc golfers to track their putting consistency. By identifying patterns, players will have the ability to leverage their stats to make the necessary modifications that will make them a better putter and, in turn, an improved player. Cue the Putting Logbook.</p>
       </section>
       <section className='resources'>
         <header className='page-header'>
@@ -17,10 +17,10 @@ export const About = () => {
         </header>
         <hr className='divider' />
         <ul className='resource-list'>
-          <li><a href='https://www.innovadiscs.com/home/disc-golf-faq/glossary-of-terms/'>Lingo</a></li>
-          <li><a href='https://allthingsdiscgolf.com/two-disc-golf-putting-tips-focus-simulation/'>Tips</a></li>
-          <li><a href='https://mainediscgolf.com/greenhorn/simple-putting-drill/'>Drills</a></li>
-          <li><a href='https://dgputtheads.com/disc-golf-putting-games'>Games</a></li>
+          <li><a href='https://www.innovadiscs.com/home/disc-golf-faq/glossary-of-terms/' target='_blank' rel='noopener noreferrer'>Lingo</a></li>
+          <li><a href='https://allthingsdiscgolf.com/two-disc-golf-putting-tips-focus-simulation/' target='_blank' rel='noopener noreferrer'>Tips</a></li>
+          <li><a href='https://mainediscgolf.com/greenhorn/simple-putting-drill/' target='_blank' rel='noopener noreferrer'>Drills</a></li>
+          <li><a href='https://dgputtheads.com/disc-golf-putting-games' target='_blank' rel='noopener noreferrer'>Games</a></li>
         </ul>
       </section>
     </article>
