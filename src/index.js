8d3ffda2f41eba33a38a5309569259317c1f77e9
Daniel Humphreys <dhumphreys.co@gmail.com>
2018-10-30 11:35:15 -0700

    Expand metatag list

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css'
 import PuttingLogApp from './PuttingLogApp'
 import * as serviceWorker from './serviceWorker'
 
-const metaTagsList = ['downhill', 'uphill', 'headwind', 'tailwind', 'left to right wind', 'right to left wind', 'bullseye', 'c1', 'c2', 'outside c2']
+const metaTagsList = ['downhill', 'uphill', 'headwind', 'tailwind', 'left to right wind', 'right to left wind', 'bullseye', 'c1', 'c2', 'outside c2', 'spit out', 'cut-through', 'straddle', 'jump putt', 'turbo', 'skip-in', 'forehand', 'backhand', 'hyzer', 'anhyzer']
 
 ReactDOM.render(<PuttingLogApp metaTagsList={metaTagsList} />, document.getElementById('root'))
 
