62fdfee101a6172dda700f22f83bec187903be45
Daniel Humphreys <dhumphreys.co@gmail.com>
2018-11-01 16:15:01 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/disc-golf-putting-logbook/precache-manifest.2e1e23457ca1bc951971e71ec5c668b6.js"
+  "/putting-logbook/precache-manifest.346c0474fa96546e0cad94983ea14409.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/disc-golf-putting-logbook/index.html", {
+workbox.routing.registerNavigationRoute("/putting-logbook/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
