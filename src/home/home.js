4eba4f44de17032cede1c1ebc81e8d08ec0cf14a
Daniel Humphreys <dhumphreys.co@gmail.com>
2018-11-02 17:39:20 -0700

    Update home page layout

--- src/home/home.js
+++ src/home/home.js
@@ -12,12 +12,15 @@ import './home.css'
  */
 export const Home = () => {
   return (
-    <div id='home' className='container-fluid mt-4 d-flex flex-column align-items-center animated zoomIn fast'>
-      <h1 className='app-title blue-grey-text'>Disc Golf Putting Logbook</h1>
-      <h2 className='app-secondary-title grey-text'>Log Putts, View Stats, Improve Your Game, Repeat</h2>
-      <div>
-        <Link to={process.env.PUBLIC_URL + '/about'} className='btn btn-default Ripple-parent'>Learn More</Link>
-        <Link to={process.env.PUBLIC_URL + '/log'} className='btn btn-default Ripple-parent'>Get Started</Link>
+    <div id='home' className='container-fluid d-flex justify-content-center align-items-center'>
+      <div id='home-overlay' className='pattern-1' />
+      <div id='home-content' className='animated fadeInUp fast d-flex flex-column align-items-center'>
+        <h1 className='app-title white-text'>Disc Golf Putting Logbook</h1>
+        <h2 className='app-secondary-title'>Log Putts, View Stats, Improve Your Game, Repeat</h2>
+        <div>
+          <Link to={process.env.PUBLIC_URL + '/about'} className='btn btn-default Ripple-parent'>Learn More</Link>
+          <Link to={process.env.PUBLIC_URL + '/log'} className='btn btn-default Ripple-parent'>Get Started</Link>
+        </div>
       </div>
     </div>
   )
